f56bd295652da8847ab4e8d4df774f6e35593beb
Jack Lund <jacklund920@gmail.com>
2022-05-13 18:55:49 -0500

    backend route for movie genres complete

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
-router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+
+router.get('/:id', (req, res) => {
+  const queryText = `select genres.name from genres
+  join movies_genres on genres.id = movies_genres.genre_id
+  join movies on movies.id = movies_genres.movie_id
+  where movies.id = $1`;
+  queryValues = [req.params.id];
+
+  pool.query(queryText, queryValues)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all movies', err);
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
